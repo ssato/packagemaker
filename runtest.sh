bc069d7a523e5c860fc16056fb3be142ca564d8c
Satoru SATOH <satoru.satoh@gmail.com>
2011-06-16 22:45:31 +0900

    renamed

--- runtest.sh
+++ runtest.sh
@@ -4,7 +4,7 @@ set -e
 
 topdir=${0%/*}
 
-if test $# -gt 1; then
+if test -n "$@"; then
     PYTHONPATH=$topdir nosetests -c $topdir/tests/nose.cfg $@
 else
     PYTHONPATH=$topdir nosetests -c $topdir/tests/nose.cfg -w $topdir/tests/
