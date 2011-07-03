d6cbb9ef52ec79ccbdeec9aceb088c66c648bfaf
Satoru SATOH <satoru.satoh@gmail.com>
2011-07-03 20:03:34 +0900

    added tests.utils.py and make test cases utilizes it

--- tests/00_single_file_filelist_tgz.py
+++ tests/00_single_file_filelist_tgz.py
@@ -14,24 +14,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from pmaker.cui import main as cui_main
 from pmaker.utils import rm_rf
+from tests.utils import *
 
 import glob
 import logging
 import os
 import os.path
-import random
-import sys
-import tempfile
 import unittest
 
 
 
-class Test_cui_main__single_file_tgz(unittest.TestCase):
+class Test_00_single_file_tgz(unittest.TestCase):
 
     def setUp(self):
-        self.workdir = tempfile.mkdtemp(dir="/tmp", prefix="pmaker-tests")
+        self.workdir = helper_create_tmpdir()
         self.pmworkdir = os.path.join(self.workdir, "pm")
         self.listfile = os.path.join(self.workdir, "files.list")
         self.template_path = os.path.join(os.getcwd(), "templates")
@@ -41,32 +38,24 @@ class Test_cui_main__single_file_tgz(unittest.TestCase):
     def tearDown(self):
         rm_rf(self.workdir)
 
-    def helper_run_with_args(self, args):
-        try:
-            cui_main(args.split())
-        except SystemExit:
-            pass
-
-        #self.assertTrue(...something_to_confirm_access...)
-
     def test_00_generated_file(self):
         target = os.path.join(self.workdir, "aaa.txt")
         os.system("touch " + target)
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "argv0 --name foobar --template-path %s -w %s --format %s %s" % \
+        args = "--name foobar --template-path %s -w %s --format %s %s" % \
             (self.template_path, self.pmworkdir, "tgz", self.listfile)
-        self.helper_run_with_args(args)
+        helper_run_with_args(args)
 
     def test_01_system_file(self):
-        target = random.choice([f for f in glob.glob("/etc/*") if os.path.isfile(f)])
+        target = helper_random_system_files(1)
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "argv0 --name foobar --template-path %s -w %s --format %s %s" % \
+        args = "--name foobar --template-path %s -w %s --format %s %s" % \
             (self.template_path, self.pmworkdir, "tgz", self.listfile)
-        self.helper_run_with_args(args)
+        helper_run_with_args(args)
 
 
 # vim: set sw=4 ts=4 expandtab:
---
+++ tests/utils.py
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2011 Satoru SATOH <satoru.satoh @ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from pmaker.cui import main as cui_main
+
+import glob
+import os
+import os.path
+import random
+import sys
+import tempfile
+import unittest
+
+
+
+def helper_create_tmpdir(dir="/tmp", prefix="pmaker-system-tests-"):
+    return tempfile.mkdtemp(dir=dir, prefix=prefix)
+
+
+def helper_is_deb_based_systems():
+    if os.path.exists("/etc/debian_version"):
+        return True
+    else:
+        print >> sys.stderr, "This system does not look a Deb-based system."
+        return False
+
+
+def helper_is_rpm_based_system():
+    if os.path.exists("/var/lib/rpm"):
+        return True
+    else:
+        print >> sys.stderr, "This system does not look a RPM-based system."
+        return False
+
+
+def helper_run_with_args(args):
+    try:
+        cui_main(["dummy_argv_0"] + args.split())
+    except SystemExit:
+        pass
+
+
+def helper_random_system_files(num=1):
+    if num == 1:
+        return random.choice([f for f in glob.glob("/etc/*") if os.path.isfile(f)])
+    else:
+        return random.sample([f for f in glob.glob("/etc/*") if os.path.isfile(f)], num)
+
+
+# vim: set sw=4 ts=4 expandtab:
