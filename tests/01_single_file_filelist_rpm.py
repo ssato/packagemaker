5d8c7c00b6461ccf0f85a551a5a8df345d038a51
Satoru SATOH <satoru.satoh@gmail.com>
2011-07-04 13:14:24 +0900

    some refactorings

--- tests/01_single_file_filelist_rpm.py
+++ tests/01_single_file_filelist_rpm.py
@@ -15,8 +15,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 from pmaker.utils import rm_rf
+from pmaker.environ import get_arch
 from tests.utils import *
 
+import glob
 import logging
 import os
 import os.path
@@ -29,16 +31,28 @@ class Test_00_single_file_rpm(unittest.TestCase):
 
     def setUp(self):
         self.workdir = helper_create_tmpdir()
-        self.pmworkdir = os.path.join(self.workdir, "pm")
         self.listfile = os.path.join(self.workdir, "files.list")
         self.template_path = os.path.join(os.getcwd(), "templates")
+
+        p = PKG_0
+        p["tpath"] = self.template_path
+        p["wdir"] = self.workdir
+        p["fmt"] = "rpm"
+
+        args = "--name %(name)s --template-path %(tpath)s -w %(wdir)s --format %(fmt)s " % p
+        self.args = args + self.listfile
+
+        pnv = "%(name)s-%(version)s" % p
+
+        rpm_pattern = os.path.join(self.workdir, pnv, pnv + "*")
+        self.rpm_pattern = rpm_pattern
 
         logging.getLogger().setLevel(logging.WARN) # suppress log messages
 
     def tearDown(self):
         rm_rf(self.workdir)
 
-    def test_00_generated_file__wo_mock(self):
+    def test_00_generated_file__srpm(self):
         if not helper_is_rpm_based_system():
             return
 
@@ -47,11 +61,12 @@ class Test_00_single_file_rpm(unittest.TestCase):
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "--name foobar --template-path %s -w %s --format %s --no-mock %s" % \
-            (self.template_path, self.pmworkdir, "rpm", self.listfile)
-        helper_run_with_args(args)
+        helper_run_with_args(self.args + " --upto sbuild")
 
-    def test_01_generated_file__w_mock(self):
+        rpms = glob.glob(self.rpm_pattern + "src.rpm")
+        self.assertTrue(len(rpms) > 0)
+
+    def test_01_generated_file__wo_mock(self):
         if not helper_is_rpm_based_system():
             return
 
@@ -60,11 +75,26 @@ class Test_00_single_file_rpm(unittest.TestCase):
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "--name foobar --template-path %s -w %s --format %s %s" % \
-            (self.template_path, self.pmworkdir, "rpm", self.listfile)
-        helper_run_with_args(args)
+        helper_run_with_args(self.args + " --upto build --no-mock")
 
-    def test_02_system_file__no_conflicts__wo_mock(self):
+        rpms = glob.glob(self.rpm_pattern + "noarch.rpm")
+        self.assertTrue(len(rpms) > 0)
+
+    def test_02_generated_file__w_mock(self):
+        if not helper_is_rpm_based_system():
+            return
+
+        target = os.path.join(self.workdir, "aaa.txt")
+        os.system("touch " + target)
+
+        open(self.listfile, "w").write("%s\n" % target)
+
+        helper_run_with_args(self.args + " --upto build")
+
+        rpms = glob.glob(self.rpm_pattern + "noarch.rpm")
+        self.assertTrue(len(rpms) > 0)
+
+    def test_03_system_file__no_conflicts__wo_mock(self):
         if not helper_is_rpm_based_system():
             return
 
@@ -77,16 +107,16 @@ class Test_00_single_file_rpm(unittest.TestCase):
             "/etc/resolv.conf"
         ]
 
-        target = random.choice([f for f in targets if os.path.exists(f)])
+        target = random.choice([f for f in targets if os.path.exists(f) and os.access(f, os.R_OK)])
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "--name foobar --template-path %s -w %s --format %s --no-mock %s" % \
-            (self.template_path, self.pmworkdir, "rpm", self.listfile)
-        helper_run_with_args(args)
+        helper_run_with_args(self.args + " --upto build")
 
+        rpms = glob.glob(self.rpm_pattern + "noarch.rpm")
+        self.assertTrue(len(rpms) > 0)
 
-    def test_03_system_file__conflicts__wo_mock(self):
+    def test_04_system_file__conflicts__wo_mock(self):
         if not helper_is_rpm_based_system():
             return
 
@@ -100,13 +130,28 @@ class Test_00_single_file_rpm(unittest.TestCase):
             "/etc/rc.d/rc.local",
         ]
 
-        target = random.choice([f for f in targets if os.path.exists(f)])
+        target = random.choice([f for f in targets if os.path.exists(f) and os.access(f, os.R_OK)])
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        args = "--name foobar --template-path %s -w %s --format %s --no-mock %s" % \
-            (self.template_path, self.pmworkdir, "rpm", self.listfile)
-        helper_run_with_args(args)
+        helper_run_with_args(self.args + " --upto build")
+
+        rpms = glob.glob(self.rpm_pattern + "noarch.rpm")
+        self.assertTrue(len(rpms) > 0)
+
+    def test_05_generated_file__wo_mock__w_arch(self):
+        if not helper_is_rpm_based_system():
+            return
+
+        target = os.path.join(self.workdir, "aaa.txt")
+        os.system("touch " + target)
+
+        open(self.listfile, "w").write("%s\n" % target)
+
+        helper_run_with_args(self.args + " --upto build --no-mock --arch")
+
+        rpms = glob.glob(self.rpm_pattern + ".%s.rpm" % get_arch())
+        self.assertTrue(len(rpms) > 0)
 
 
 # vim: set sw=4 ts=4 expandtab:
