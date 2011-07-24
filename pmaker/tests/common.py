eabee8bb01bbf92e7658e05d655c2091b993c3fc
Satoru SATOH <satoru.satoh@gmail.com>
2011-07-25 08:29:48 +0900

    started to gather common test routines

---
+++ pmaker/tests/common.py
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2011 Satoru SATOH <satoru.satoh @ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+from pmaker.utils import rm_rf
+
+import tempfile
+
+
+
+def setup_workdir():
+    return tempfile.mkdtemp(dir="/tmp", prefix="pmaker-tests")
+
+
+def cleanup_workdir(workdir):
+    rm_rf(workdir)
+
+
+# vim: set sw=4 ts=4 expandtab:
