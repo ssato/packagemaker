6c2a60fd8c12d2904c0b16e3f0fae0234e8489ce
Satoru SATOH <satoru.satoh@gmail.com>
2011-08-06 06:55:42 +0900

    check # of arguments instead of checking if '' is empty

--- runtest.sh
+++ runtest.sh
@@ -4,7 +4,7 @@ set -e
 
 topdir=${0%/*}
 
-if test -n "$@"; then
+if test $# -gt 0; then
     PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg $@
 else
     PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg -w $topdir/tests/
