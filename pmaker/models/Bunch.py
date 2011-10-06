8cab017bd298d3cd3191416cc69b3c8e9f1e4596
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-06 22:47:24 +0900

    added a class implements 'Bunch Pattern' and its test code

---
+++ pmaker/models/Bunch.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2011 Satoru SATOH <satoru.satoh @ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+class Bunch(dict):
+    """
+    Simple class implements 'Bunch Pattern'.
+
+    @see http://ruslanspivak.com/2011/06/12/the-bunch-pattern/
+    """
+
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+# vim:sw=4 ts=4 expandtab:
