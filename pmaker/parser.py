12fdd26d1bf9f95d60d57116ff2a98123f5eea9c
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-11 13:32:43 +0900

    added new module to consolidate parsing methods

---
+++ pmaker/parser.py
@@ -0,0 +1,102 @@
+#
+# Copyright (C) 2011 Satoru SATOH <satoru.satoh @ gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import itertools
+import logging
+import re
+
+
+INT_PATTERN = re.compile(r"^(\d|([1-9]\d+))$")
+BOOL_PATTERN = re.compile(r"^(true|false)$", re.I)
+STR_PATTERN = re.compile(r"^['\"](.*)['\"]$")
+
+
+def parse_single(s):
+    """
+    Very simple parser to parse expressions represent some single values.
+
+    >>> assert 0 == parse_single("0")
+    >>> assert 123 == parse_single("123")
+    >>> assert True == parse_single("True")
+    >>> assert "a string" == parse_single("a string")
+    >>> assert "0.1" == parse_single("0.1")
+    >>> parse_single("    a string contains extra whitespaces     ")
+    'a string contains extra whitespaces'
+    """
+    def matched(pat, s):
+        return pat.match(s) is not None
+
+    s = s.strip()
+
+    if not s:
+        return ""
+
+    if matched(BOOL_PATTERN, s):
+        return bool(s)
+
+    if matched(INT_PATTERN, s):
+        return int(s)
+
+    if matched(STR_PATTERN, s):
+        return s[1:-1]
+
+    return s
+
+
+def parse_list(s, sep=","):
+    """
+    Simple parser to parse expressions reprensent some list values.
+
+    :param sep: Char to separate items of list.
+
+    >>> assert parse_list("") == []
+    >>> assert parse_list("a,b") == ["a", "b"]
+    >>> assert parse_list("1,2") == [1, 2]
+    >>> assert parse_list("a,b,") == ["a", "b"]
+    """
+    return [parse_single(x) for x in s.split(sep) if x]
+
+
+def parse_attrlist(s, avs_sep=":", vs_sep=",", as_sep=";"):
+    """
+    Simple parser to parse expressions in the form of
+    [ATTR1:VAL0,VAL1,...;ATTR2:VAL0,VAL2,..].
+
+    :param s: input string
+    :param avs_sep:  char to separate attribute and values
+    :param vs_sep:  char to separate values
+    :param as_sep:  char to separate attributes
+
+    >>> parse_attrlist("requires:bash,zsh")
+    [('requires', ['bash', 'zsh'])]
+    >>> parse_attrlist("obsoletes:sysdata;conflicts:sysdata-old")
+    [('obsoletes', ['sysdata']), ('conflicts', ['sysdata-old'])]
+    """
+    def attr_and_values(s):
+        for rel in parse_list(s, as_sep):
+            if avs_sep not in rel or rel.endswith(avs_sep):
+                continue
+
+            (_attr, _values) = parse_list(rel, avs_sep)
+            _values = parse_list(_values, vs_sep)
+
+            if _values:
+                yield (_attr, _values)
+
+    return [(a, vs) for a, vs in attr_and_values(s)]
+
+
+# vim:sw=4 ts=4 et:
