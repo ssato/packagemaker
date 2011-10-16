3886500a77bae42131e52ee197b67fcc927c5ba9
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-16 17:25:28 +0900

    renamed and related changes

--- pmaker/backend/AutotoolsSingleDeb.py
+++ pmaker/backend/AutotoolsSingleDeb.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import pmaker.backend.AutotoolsSingle as A
+import pmaker.backend.AutotoolsSingleTgz as A
 import pmaker.rpmutils as R
 import pmaker.utils as U
 
