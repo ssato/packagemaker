b60ab62ccb1c74f93f64cb93f61b330c040bef93
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-17 01:37:38 +0900

    make it checks with pep8 in python-pep8

--- runtest.sh
+++ runtest.sh
@@ -5,6 +5,7 @@ set -e
 topdir=${0%/*}
 
 if test $# -gt 0; then
+    PYTHONPATH=$topdir pep8 $@
     PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg $@
 else
     PYTHONPATH=$topdir nosetests -c $topdir/nose.cfg -w $topdir/tests/
