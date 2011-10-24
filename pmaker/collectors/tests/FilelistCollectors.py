3bd423aa851479832d7a1aa91e2d810703ecf2f2
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-25 01:25:07 +0900

    added a testcase for pmaker.collectors.FilelistCollectors.FilelistCollector.collect

--- pmaker/collectors/tests/FilelistCollectors.py
+++ pmaker/collectors/tests/FilelistCollectors.py
@@ -19,6 +19,7 @@ from pmaker.collectors.FilelistCollectors import FilelistCollector, \
 from pmaker.tests.common import setup_workdir, cleanup_workdir
 
 import pmaker.options as O
+import pmaker.collectors.Filters as Filters
 import pmaker.models.FileObjectFactory as Factory
 import pmaker.utils as U
 
@@ -149,6 +150,47 @@ class Test_01_FilelistCollector(unittest.TestCase):
         fos_ref = sorted(
             Factory.create(p, False, checksum=U.checksum(p)) for p in paths_ref
         )
+
+        self.assertEquals(sorted(fos), fos_ref)
+
+    def test_04_collect__multi_real_files(self):
+        paths = [
+            "/etc/auto.*",
+            "#/etc/aliases.db",
+            "/etc/httpd/conf.d",
+            "/etc/httpd/conf.d/*",
+            "/etc/modprobe.d/*",
+            "/etc/rc.d/init.d",
+            "/etc/rc.d/rc",
+            "/etc/resolv.conf",
+            "/etc/reslv.conf",
+            "/etc/grub.conf",
+            "/usr/share/automake-*/am/*.am",
+            "/var/run/*",
+        ]
+
+        paths_ref = U.unique(
+            U.concat(glob.glob(p) for p in paths if not p.startswith("#"))
+        )
+
+        listfile = os.path.join(self.workdir, "file.list")
+        config = init_config(listfile)
+
+        open(listfile, "w").write("\n".join(paths))
+
+        collector = FilelistCollector(listfile, config)
+
+        fos = collector.collect()
+
+        filters = [
+            Filters.UnsupportedTypesFilter(), Filters.ReadAccessFilter(),
+        ]
+        fos_ref = sorted(
+            Factory.create(p, False, checksum=U.checksum(p)) for p in paths_ref
+        )
+        fos_ref = [
+            f for f in fos_ref if not any(filter(f) for filter in filters)
+        ]
 
         self.assertEquals(sorted(fos), fos_ref)
 
