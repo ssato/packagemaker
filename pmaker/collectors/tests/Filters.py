d12be9cddb3872456d1c8573ed1fb746060d662f
Satoru SATOH <satoru.satoh@gmail.com>
2011-10-28 02:21:42 +0900

    added testcases for FileObjects

--- pmaker/collectors/tests/Filters.py
+++ pmaker/collectors/tests/Filters.py
@@ -19,7 +19,12 @@ from pmaker.models.FileInfo import FileInfo, UnknownInfo
 from pmaker.models.FileInfoFactory import FileInfoFactory
 from pmaker.utils import checksum
 
+import pmaker.models.FileObjectFactory as Factory
+
+import glob
 import os
+import os.path
+import pwd
 import random
 import unittest
 
@@ -37,43 +42,75 @@ class TestUnsupportedTypesFilter(unittest.TestCase):
         fi = UnknownInfo("/dummy/path")
         self.assertTrue(self.filter.pred(fi))
 
+    def test_pred__supported__fileobjects(self):
+        paths = [
+            "/etc/resolv.conf", "/etc/hosts",  # normal files
+            "/etc",  # dirs
+            "/etc/rc",  "/etc/init.d", "/etc/grub.conf"  # symlinks
+        ]
+        paths = [p for p in paths if os.path.exists(p)]
+
+        for p in paths:
+            fo = Factory.create(p)
+            self.assertFalse(self.filter.pred(fo))
+
+    def test_pred__unsupported__fileobjects(self):
+        username = pwd.getpwuid(os.getuid()).pw_name
+        paths = [
+            "/dev/null", "/dev/zero"  # device files
+        ] + glob.glob("/tmp/orbit-%s/linc-*" % username)[:1]  # socket
+
+        paths = [p for p in paths if os.path.exists(p)]
+
+        for p in paths:
+            fo = Factory.create(p)
+            self.assertTrue(self.filter.pred(fo))
+
 
 class TestReadAccessFilter(unittest.TestCase):
 
-    def setUp(self):
-        self.filter = ReadAccessFilter()
+    def test_pred__not_permitted_to_read_or_to_be_created(self):
+        if os.getuid() == 0:
+            print >> sys.stderr, "You look root and cannot test this. Skipped"
+            return
+
+        filter = ReadAccessFilter()
         path = random.choice(
-            [p for p in ("/etc/at.deny", "/etc/securetty", "/etc/sudoer", \
-                    "/etc/shadow", "/etc/grub.conf") \
+            [p for p in ("/etc/at.deny",
+                         "/etc/securetty",
+                         "/etc/sudoer",
+                         "/etc/shadow",
+                         "/etc/grub.conf") \
                 if os.path.exists(p) and not os.access(p, os.R_OK)]
-        )
-        self.fi = FileInfoFactory().create(path)
-
-    def test_pred__dont_have_read_access(self):
-        if os.getuid() == 0:
-            print >> sys.stderr, "You look root and cannot test this. Skipped"
-            return
-
-        self.assertTrue(self.filter.pred(self.fi))
-
-    def test_pred__dont_have_read_access_but_to_be_created(self):
-        if os.getuid() == 0:
-            print >> sys.stderr, "You look root and cannot test this. Skipped"
-            return
-
-        fi = self.fi
-        fi.create = True
-        self.assertFalse(self.filter.pred(fi))
-
-    def test_pred__have_read_access(self):
-        path = random.choice(
-            [p for p in \
-                ("/etc/hosts", "/etc/resolv.conf", "/etc/sysctl.conf") \
-                    if os.path.exists(p) and os.access(p, os.R_OK)]
         )
 
         fi = FileInfoFactory().create(path)
-        self.assertFalse(self.filter.pred(fi))
+        fo = Factory.create(path)
+
+        self.assertTrue(filter.pred(fi))
+        self.assertTrue(filter.pred(fo))
+
+        fi.create = True
+        fo.create = True
+
+        self.assertFalse(filter.pred(fi))
+        self.assertFalse(filter.pred(fo))
+
+    def test_pred__permitted_to_read(self):
+        filter = ReadAccessFilter()
+        path = random.choice(
+            [p for p in ("/etc/hosts",
+                         "/etc/resolv.conf",
+                         "/etc/sysctl.conf",
+                    ) if os.path.exists(p) and os.access(p, os.R_OK)
+            ]
+        )
+
+        fi = FileInfoFactory().create(path)
+        self.assertFalse(filter.pred(fi))
+
+        fo = Factory.create(path)
+        self.assertFalse(filter.pred(fo))
 
 
 # vim:sw=4 ts=4 et:
