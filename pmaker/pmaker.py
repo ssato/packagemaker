22e473bf84db5ca6fcaf60b618307f4ec7903cb0
Satoru SATOH <satoru.satoh@gmail.com>
2011-11-10 10:28:56 +0900

    renamed

--- pmaker/pmaker.py
+++ pmaker/pmaker.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import pmaker.options as Opt
 import pmaker.collectors.FilelistCollectors as Collectors
 import pmaker.backend.registry as Backends
 
@@ -21,7 +22,7 @@ import sys
 
 
 def main(argv=sys.argv):
-    o = Options()
+    o = Opt.Options()
     (opts, args) = o.parse_args(argv[1:])
 
     collector = Collectors.AnyFilelistCollector(args[0], opts)
