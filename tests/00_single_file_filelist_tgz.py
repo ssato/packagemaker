da17477e3c4350d40e7b33e3e8ebcbeb92b185db
Satoru SATOH <satoru.satoh@gmail.com>
2011-12-05 03:24:57 +0900

    started to implements system tests

--- tests/00_single_file_filelist_tgz.py
+++ tests/00_single_file_filelist_tgz.py
@@ -14,83 +14,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from pmaker.utils import rm_rf
-from tests.utils import *
+import tests.common as TC
+import pmaker.environ as E
+import pmaker.tests.common as C
 
-import copy
-import glob
-import logging
-import os
 import os.path
 import unittest
 
 
-
-class Test_00_single_file_tgz(unittest.TestCase):
+class Test_00_autotools_single(unittest.TestCase):
 
     def setUp(self):
-        self.workdir = helper_create_tmpdir()
-        self.pmworkdir = os.path.join(self.workdir, "pm")
+        (name, pversion) = ("foo", "0.0.3")
+
+        (self.workdir, args) = TC.setup(
+            ["--name", name, "--pversion", pversion]
+        )
         self.listfile = os.path.join(self.workdir, "files.list")
-        self.template_path = os.path.join(os.getcwd(), "templates")
+        comp_ext = E.Env().compressor.extension
 
-        p = self.p = PKG_0
-        p["tpath"] = self.template_path
-        p["wdir"] = self.pmworkdir
-        p["fmt"] = "tgz"
+        self.args = args + ["--backend", "autotools.single.tgz", self.listfile]
 
-        args = "--name %(name)s --template-path %(tpath)s -w %(wdir)s --format %(fmt)s " % p
-        self.args = args + self.listfile
-
-        pnv = "%(name)s-%(version)s" % p
-
-        tgz = os.path.join(self.pmworkdir, pnv, pnv + ".tar." + helper_get_compressor_ext())
-        self.tgz = tgz
-
-        logging.getLogger().setLevel(logging.WARNING) # suppress log messages
+        pn = "%s-%s" % (name, pversion)
+        self.tgz = os.path.join(self.workdir, pn, pn + ".tar." + comp_ext)
 
     def tearDown(self):
-        rm_rf(self.workdir)
+        #C.cleanup_workdir(self.workdir)
+        pass
 
     def test_00_generated_file(self):
         target = os.path.join(self.workdir, "aaa.txt")
-        os.system("touch " + target)
 
+        open(target, "w").write("\n")
         open(self.listfile, "w").write("%s\n" % target)
 
-        helper_run_with_args(self.args)
+        TC.run_w_args(self.args, self.workdir)
 
         self.assertTrue(os.path.exists(self.tgz))
-
-    def test_00_generated_file__with_pversion(self):
-        p = copy.copy(self.p)
-
-        pversion = "1.2.3"
-        p["version"] = pversion
-
-        pnv = "%(name)s-%(version)s" % p
-
-        tgz = os.path.join(self.pmworkdir, pnv, pnv + ".tar." + helper_get_compressor_ext())
-
-        target = os.path.join(self.workdir, "bbb.txt")
-        os.system("touch " + target)
-
-        open(self.listfile, "w").write("%s\n" % target)
-
-        helper_run_with_args(self.args + " --pversion " + pversion + " ")
-
-        print "tgz=" + tgz
-        self.assertTrue(os.path.exists(tgz))
 
     def test_01_system_file(self):
-        target = helper_random_system_files(1)
+        target = TC.get_random_system_files(1, "/etc/*")
 
         open(self.listfile, "w").write("%s\n" % target)
 
-        helper_run_with_args(self.args)
+        TC.run_w_args(self.args, self.workdir)
 
-        print "tgz=" + self.tgz
         self.assertTrue(os.path.exists(self.tgz))
 
 
-# vim: set sw=4 ts=4 expandtab:
+# vim:sw=4 ts=4 et:
