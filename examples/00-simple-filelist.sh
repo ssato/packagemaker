24637f56cc12839c3b05fbd6e4e9c1c6c0502702
Satoru SATOH <satoru.satoh@gmail.com>
2011-12-25 06:41:04 +0900

    updated example logs

--- examples/00-simple-filelist.sh
+++ examples/00-simple-filelist.sh
@@ -12,6 +12,6 @@ cat $dir/simple-files.list
 pmaker -n sysdata --pversion 0.1 --no-mock -w /tmp/0 $dir/simple-files.list
 ls /tmp/0
 ls /tmp/0/sysdata-0.1/
-rpm -qlp /tmp/0/sysdata-0.1/sysdata-0.1-1.fc14.noarch.rpm
-rpm -qlp /tmp/0/sysdata-0.1/sysdata-overrides-0.1-1.fc14.noarch.rpm
-rpm -qp --scripts /tmp/0/sysdata-0.1/sysdata-overrides-0.1-1.fc14.noarch.rpm
+rpm -qlp /tmp/0/sysdata-0.1/sysdata-0.1-1.*.noarch.rpm
+rpm -qlp /tmp/0/sysdata-0.1/sysdata-overrides-0.1-1.*.noarch.rpm
+rpm -qp --scripts /tmp/0/sysdata-0.1/sysdata-overrides-0.1-1.*.noarch.rpm
