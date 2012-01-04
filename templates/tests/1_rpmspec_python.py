611acff35262508fc1cd9ad04f27ffb61708d455
Satoru SATOH <satoru.satoh@gmail.com>
2012-01-05 00:06:15 +0900

    added simple test cases for rpmspec templates

---
+++ templates/tests/1_rpmspec_python.py
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2011 Satoru SATOH <ssato at redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from pmaker.tests.common import selfdir
+
+import pmaker.tenjinwrapper as TW
+import os.path
+import unittest
+
+
+def tmplpath(fname):
+    return os.path.abspath(
+        os.path.join(selfdir(), "../../templates/1/rpmspec", fname)
+    )
+
+
+class Test_00(unittest.TestCase):
+
+    def test_01_package_spec(self):
+        tmpl = tmplpath("python/package.spec")
+
+        context = dict(
+            name="python-foo-bar-baz",
+            version="1.0",
+            license="MIT",
+            url="http://example.com/foo-bar-baz",
+            summary="Python foo bar baz library",
+            description="Python foo bar baz library",
+        )
+
+        c = TW.template_compile(tmpl, context)
+
+
+# vim:sw=4 ts=4 et:
