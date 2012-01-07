203fd9b0ada5a863cdd952ed52fc18cb9f0ee170
Satoru SATOH <satoru.satoh@gmail.com>
2012-01-08 02:26:17 +0900

    removed dependency to BeautifulSoup

--- pmaker/rpmspec/python.py
+++ pmaker/rpmspec/python.py
@@ -17,7 +17,6 @@
 import pmaker.models.Bunch as B
 import pmaker.utils as U
 
-import BeautifulSoup
 import logging
 import os.path
 import re
@@ -26,58 +25,40 @@ import sys
 
 def get_download_url(name, base_url="pypi.python.org/pypi/"):
     """
+
+    pi_url example: http://pypi.python.org/pypi/pyev/
+
+    search pattern: 
+        '<a href="<URL>#md5=..."><NAME>-<VERSION>.tar.gz</a>
+
+        where <X> = Initial character of name
+              <NAME> = name
+              <VERSION> = version
+              <DBASE_URL> = "http://pypi.python.org/packages/source"
+              <URL> = <DBASE_URL>/<X>/<NAME>/<NAME>-<VERSION>.tar.gz
+
     """
     pi_url = base_url + name
     page = U.urlread(pi_url)
 
     if page is None:
         logging.warn("Could not get the www page content: " + name)
-
-    soup = BeautifulSoup.BeautifulSoup(page)
-
-    reg = re.compile(r"(http://pypi.python.org/packages/source/.*)#md5=.*")
-    t = soup.find("a", attrs={"href": reg})
-    if t is None:
-        logging.warn("Could not resolve download url: " + name)
         return None
 
-    download = reg.match(dict(t.attrs)["href"]).groups()[0]
-    return download
-
-
-def parse_pypi_page(name, base_url="pypi.python.org/pypi/"):
-    """
-    """
-    pi_url = base_url + name
-    page = U.urlread(pi_url)
-
-    if page is None:
-        logging.warn("Could not get the www page content: " + name)
-
-    soup = BeautifulSoup.BeautifulSoup(page)
-
-    summary = soup.find("meta", attrs={"name": "description"})
-    if summary is not None:
-        summary = dict(summary).attrs).get("content", "")
-
-    url = None
-    t = soup.find("strong", text=re.compile(r"^home.*page.*", re.IGNORECASE))
-    if t is not None:
-        href = t.parent.parent.find("a").attrs).get("href")
-        if href is not None:
-            url = dict(href.attrs).get("href", None)
-
-    download = None
-    reg = re.compile(r"(http://pypi.python.org/packages/source/.*)#md5=.*")
-    t = soup.find("a", attrs={"href": reg})
-    if t is not None:
-        download = reg.match(dict(t.attrs)["href"]).groups()[0]
-
-    return Bunch(
-        name=name,
-        url=url,
-        download=download,
+    reg = re.compile(
+        r".*(http://pypi.python.org/packages/source/[^#]+)#md5=.*"
     )
+
+    ls = [l for l in page.split("\n") if reg.match(l)]
+    if not ls:
+        logging.warn("Could not get the download url: " + name)
+        return None
+
+    m = reg.match(ls[0])
+    assert m is not None, str(ls)
+
+    download = m.groups()[0]
+    return download
 
 
 # vim:sw=4 ts=4 et:
