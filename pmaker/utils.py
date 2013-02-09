d2017f7b1a31dc8cd086b2371ae9d9b4eb04d2d6
Satoru SATOH <satoru.satoh@gmail.com>
2013-02-09 14:13:39 +0900

    Removed unused class 'memoized'

--- pmaker/utils.py
+++ pmaker/utils.py
@@ -139,43 +139,6 @@ def memoize(fn):
     return wrapped
 
 
-class memoized(object):
-    """Decorator that caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned, and
-    not re-evaluated.
-
-    Originally came from
-    http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize.
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        try:
-            return self.cache[args]
-
-        except KeyError:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-        except TypeError:
-            # uncachable -- for instance, passing a list as an argument.
-            # Better to not cache than to blow up entirely.
-            return self.func(*args)
-
-    def __repr__(self):
-        """Return the function's docstring.
-        """
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        """Support instance methods.
-        """
-        return curry(self.__call__, obj)
-
-
 @memoize
 def checksum(filepath="", algo=sha1, buffsize=8192):
     """compute and check md5 or sha1 message digest of given file path.
