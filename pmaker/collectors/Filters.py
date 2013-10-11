6d01622b4209aeecfe85115e9757bc26e6ecbb94
Satoru SATOH <satoru.satoh@gmail.com>
2013-10-11 18:28:50 +0900

    update the copyright header

--- pmaker/collectors/Filters.py
+++ pmaker/collectors/Filters.py
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2011 Satoru SATOH <satoru.satoh @ gmail.com>
+# Copyright (C) 2011 - 2013 Red Hat, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
