99b5801063ffb4370a70ef44de2ea0b5e7e82402
Satoru SATOH <satoru.satoh@gmail.com>
2013-10-11 16:00:15 +0900

    move and some minor updates of test suite driver script and config

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -e
 
+
 testsdir=${0%/*}
-topdir=${testsdir}/..
+topdir=../$testsdir
 
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
 
@@ -10,9 +11,6 @@ if test $# -gt 0; then
     test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
     PYTHONPATH=$topdir nosetests -c $testsdir/nose.cfg $@
 else
-    for f in $(cat $topdir/tests/targets); do 
-        echo "## $f"
-        test $check_with_pep8 = 1 && pep8 ${f%%:*} || :
-        PYTHONPATH=$topdir nosetests -c $testsdir/nose.cfg $f
-    done
+    #PYTHONPATH=$topdir nosetests -c $testsdir/nose.cfg -w $testsdir
+    echo "Usage: $0 DIR_OR_PYTHON_FILE_0[ DIR_OR_PYTHON_FILE_1 ...]"
 fi
